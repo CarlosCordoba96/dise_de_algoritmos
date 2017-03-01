bccc49dbd692be93df03ba0fe9a7d3609a913fa2
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-03-01 22:42:26 +0100

    Falta corregir tiempos

--- Diseño/src/practica1/biseccion.java
+++ Diseño/src/practica1/biseccion.java
@@ -1,17 +1,47 @@
 package practica1;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class biseccion {
 
 	public static void main(String [] args) {
-		double result;
 		double error=10e-4;
-		System.out.println(2>=error);
-		String eq="(X*X*X)+10*X - 5";
-		System.out.println(recurbisec(0,1,error));
+		double start1=System.currentTimeMillis();
+		double result=(recurbisec(-10,10,error));
+		double end1=System.currentTimeMillis();
+		double start2=System.currentTimeMillis();
+		double result2=pilas(-10,10,error);
+		double end2=System.currentTimeMillis();
+		System.out.println("El tiempo del recursivo es :"+ (end1-start1)/10000);
+		System.out.println("El tiempo del pilas es : "+ (end2-start2)/10000);
+
+	}
+
+
+	static double pilas(double a,double b,double error){
+		Stack <Double> izq=new Stack<Double>();
+		Stack <Double>der=new Stack<Double>();
+		izq.push(a);
+		der.push(b);
+		double valormedio=(a+b)/2;
+		while(Math.abs(eval(valormedio))>error){
+			if(eval(valormedio)*eval(izq.peek())>0){//f(c)*f(a)>0
+				izq.push(valormedio);
+				der.push(der.peek());
+
+			}else{//eval valor medio es negativo f(c)*f(a)<0
+				izq.push(izq.peek());
+				der.push(valormedio);
+				
+				
+				
+			}
+
+			valormedio=(izq.peek()+der.peek())/2;
+		}
+		return valormedio;
 	}
 
 	static double recurbisec(double a,double b,double error) {
@@ -20,7 +50,7 @@ public class biseccion {
 		double fa = eval(a);
 		double fb = eval(b);
 		double fc=eval(valormedio);
-		if( Math.abs(fc) <= error){//solución encontrada
+		if( Math.abs(fc) <= error){//solucion encontrada
 			sol=valormedio;
 		}else{
 
@@ -33,10 +63,6 @@ public class biseccion {
 		}
 		return sol;		
 	}
-
-	
-
-
 
 	static double eval(double x){
 

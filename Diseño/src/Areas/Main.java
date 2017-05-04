9a7e2a120b4fe9be1b2a8f1ab727c8835306a3c7
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-05-04 14:25:50 +0200

    Main volumen funcionando

---
+++ Diseño/src/Areas/Main.java
@@ -0,0 +1,28 @@
+package Areas;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Random r = new Random();
+		System.out.println("Escribe el número de repeticiones");
+		int repeticiones = sc.nextInt();
+		int i = 0;
+		double valores=0;
+		double dentro, fuera;
+		dentro = fuera = 0;
+		while(i <= repeticiones){
+			double x = r.nextDouble();
+			double y = r.nextDouble();
+			double z=Math.pow(x, 2) + Math.pow(y, 2);
+			valores+=z*1;//base por altura
+			i++;
+		}
+		//resultado es igual a puntos dentro de la figura / puntos totales = volumen de la figura / volumen del cubo
+		double resultado= valores/repeticiones;
+		System.out.println("Volumen de la funcion x^2 + y^2 es: " + resultado);
+		sc.close();
+	}
+}
